28e2a2030a35690de84a8bed948390d094e3434b
ahmed <ahmedabdelnaser3010@gmail.com>
2025-05-31 23:19:41 +0200

    task three

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,7 +2,8 @@ import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
-const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
+
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
     return (
         <div className="flex flex-col h-screen">
             <Header />
@@ -12,7 +13,7 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
                     <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
                 </div>
                 <div className="grid grid-cols-3 gap-4">
-                    {users?.map((user: UserProps, key: number) => (
+                    {posts?.map((user: UserProps, key: number) => (
                         <UserCard key={key} {...user} />
                     ))}
                 </div>
@@ -23,11 +24,11 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
 
 export async function getStaticProps() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const users = await response.json()
+    const posts = await response.json()
 
     return {
         props: {
-            users
+            posts
         }
     }
 }
